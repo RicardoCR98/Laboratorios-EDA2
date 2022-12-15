5cfbe65b8bd876f28196a0ab8a4ff561de4dd18e
RicardoCR98 <gary.campana@epn.edu.ec>
2022-12-15 17:23:25 -0500

    Laboratorio 03 con Sqlite

---
+++ Lab03-Sqlite/Program.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.Data.Sqlite;
+
+namespace Lab03_Sqlite;
+class Program
+{
+    static void Main(string[] args)
+    {
+        int id = 2;
+        Console.WriteLine("Hello, World!");
+
+        using (var connection = new SqliteConnection("Data Source=hello.db"))
+        {
+            connection.Open();
+
+            var command = connection.CreateCommand();
+            command.CommandText =
+            @"
+        SELECT name
+        FROM user
+        WHERE id = $id";
+            command.Parameters.AddWithValue("$id", id);
+
+            using (var reader = command.ExecuteReader())
+            {
+                while (reader.Read())
+                {
+                    var name = reader.GetString(0);
+
+                    Console.WriteLine($"Hello, {name}!");
+                }
+            }
+        }
+    }
+}
